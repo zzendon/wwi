a6775c59ba33329c5172511f1f43f971138e1725
zzendon <s1126859@student.windesheim.nl>
2018-10-31 18:28:31 +0100

    Veranderingen aangepast

--- connectdb.php
+++ connectdb.php
@@ -2,5 +2,13 @@
 $db ="mysql:host=localhost;dbname=wideworldimporters;port=3306";
 $user = "test";
 $pass = "123";
-$pdo = new PDO($db, $user, $pass);
+    try {
+        $pdo = new PDO("$db", $user, $pass);
+            //echo een message wanner connected to database
+            //echo 'Connected to database! </br>';
+        }
+        catch(PDOException $e){
+            echo "Error!:". $e->getMessage() . "<br/>";
+            die();
+        }
 ?>
