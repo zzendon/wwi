a8791038053cc7d795344f2aff64a6ed6446020a
zzendon <s1126859@student.windesheim.nl>
2018-11-23 10:11:41 +0100

    Stars werken op product list page

--- pages/productlist_page.php
+++ pages/productlist_page.php
@@ -38,14 +38,13 @@ function getPages($stockitems) {
 }
 
 /// Get basic stock item information.
-function getCategoriesStockItemInfo($categorie_id) {
+function getCategoriesStockItemInfo($category_id) {
     $connection = getConnection();
 
-    $pro = $connection->prepare(
-        "SELECT s.StockItem" . "Id, s.StockItemName, s.RecommendedRetailPrice, r.Stars FROM stockitems s 
+    $pro = $connection->prepare("SELECT s.StockItemId, s.StockItemName, s.RecommendedRetailPrice, AVG(r.Stars) FROM stockitems s 
            JOIN stockitemstockgroups sg ON s.StockItemID = sg.StockItemId
            LEFT JOIN review r ON r.StockItemID = s.StockItemID
-           WHERE sg.StockGroupID=". $categorie_id);
+           WHERE sg.StockGroupID='$category_id' GROUP BY s.StockItemID");
 
     $pro->execute();
 
@@ -54,7 +53,7 @@ function getCategoriesStockItemInfo($categorie_id) {
         $id = $row["StockItemId"];
         $product_name = $row["StockItemName"];
         $product_price = $row["RecommendedRetailPrice"];
-        $stars = $row["Stars"];
+        $stars = $row["AVG(r.Stars)"];
 
         $product_name = remove_color_from_stockitem($product_name);
 
@@ -143,14 +142,13 @@ function getCategoriesStockItemInfo($categorie_id) {
                         <small class="text-muted">
                             <?php
                             $stars = $value["Stars"];
-
-                            for ($i = 0; $i < 5; $i++) {
-                                if ($i < $stars) {
-                                    echo "&#9733;";
-                                } else {
-                                    echo "&#9734;";
+                                for ($i = 0; $i < 5; $i++) {
+                                    if ($i < $stars) {
+                                        echo "<i class=\"fa fa-star gold\"></i>";
+                                    }else {
+                                        echo "<i class=\"fa fa-star-o\"></i>"  ;
+                                    }
                                 }
-                            }
                             ?>
                         </small>
                     </div>
