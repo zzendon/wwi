54292961dd13426ecc1e76b9f6ecee9d30ac0317
zzendon <s1126859@student.windesheim.nl>
2018-12-07 10:20:48 +0100

    fixes

--- pages/productinfo_page.php
+++ pages/productinfo_page.php
@@ -134,12 +134,12 @@ function getStockItemCountInArchive($id)
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
+                <span style="background-color: black" class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span style="background-color: black" class="sr-only">Previous</span>
             </a>
             <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
+                <span style="background-color: black" class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span style="background-color: black" class="sr-only">Next</span>
             </a>
         </div>
     </div>
